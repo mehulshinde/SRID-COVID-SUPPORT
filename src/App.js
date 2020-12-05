52226dab3aea2598aefe3c638e39a298d341b630
Swati Bhat <swatipab@andrew.cmu.edu>
2020-12-05 12:30:22 -0800

    Initial commit Volunteer signup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
+import React from 'react'
 import logo from './logo.svg';
 import './styles/App.css';
+import VolunteerSignup from "./scripts/VolunteerSignup";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component{
+        render() {
+            return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                <VolunteerSignup></VolunteerSignup>
+                </header>
+            </div>
+            );
+        }
 }
 
 export default App;
---
+++ src/scripts/VolunteerSignup.js
@@ -0,0 +1,17 @@
+import '../styles/App.css';
+import '../styles/VolunteerSignup.css';
+import React from "react";
+
+class VolunteerSignup extends React.Component{
+    render() {
+        return (
+            <div className="Volunteer-Signup-header">
+                    <form>
+                        <p>Hello</p>
+                    </form>
+            </div>
+        );
+    }
+}
+
+export default VolunteerSignup;
