a02cdfe762a811876966c08ad36251f70e6b9774
Haoyu Huang <huanghaoyu1@gmail.com>
2020-12-05 12:15:28 -0800

    added item list

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 ## Operation Instructions
 
-- To run the app in the development mode, run `npm start` in the project directory. Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+- To run the app in the development mode, run `npm start` in the project directory.\
+ Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 
 ## Technology Stack
 
--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
+import { Container } from 'react-bootstrap';
+import ItemList from './pages/ItemList.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/App.css';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container
+      style={{
+        maxWidth: '576px',
+        height: '100vh',
+        margin: 'auto',
+      }}
+    >
+      <Router>
+        <Switch>
+          <Route exact path="/" render={() => null}></Route>
+          <Route exact path="/item-list" render={() => <ItemList />}></Route>
+        </Switch>
+      </Router>
+    </Container>
   );
 }
 
