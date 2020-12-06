f2ecccc4cf2214625be51b2ad417dbcd7cdef4a6
Mehul Shinde <mehuldshinde@gmail.com>
2020-12-05 21:09:51 -0800

    Chat modifications

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,9 +1,14 @@
+import { useState } from "react";
+
 export default function Chat() {
+  const [chatTitle, setChatTitle] = useState("");
+  const [receivedMsg, setReceivedMsg] = useState([]);
+  const [sentMsg, setSentMsg] = useState([]);
   return (
     <>
       <div class="back"></div>
       <div class="chat-header"></div>
-      <div class="chat-title"></div>
+      <div class="chat-title">Title</div>
       <div class="received-message"></div>
       <div class="received-msg-timestamp"></div>
       <div class="sent-message"></div>
