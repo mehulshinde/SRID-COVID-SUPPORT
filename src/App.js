2ecddf9f324387b2ccbc3d8a2999087b1077a8c3
Mehul Shinde <mehuldshinde@gmail.com>
2020-12-05 19:13:00 -0800

    Adding Chat screen

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
-import logo from './logo.svg';
-import { Container } from 'react-bootstrap';
-import ItemList from './pages/ItemList.js';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles/App.css';
+import logo from "./logo.svg";
+import { Container } from "react-bootstrap";
+import ItemList from "./pages/ItemList.js";
+import Chat from "./pages/Chat";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/App.css";
 
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect,
-} from 'react-router-dom';
+} from "react-router-dom";
 
 function App() {
   return (
     <Container
       style={{
-        maxWidth: '576px',
-        height: '100vh',
-        margin: 'auto',
+        maxWidth: "576px",
+        height: "100vh",
+        margin: "auto",
       }}
     >
       <Router>
         <Switch>
           <Route exact path="/" render={() => null}></Route>
           <Route exact path="/item-list" render={() => <ItemList />}></Route>
+          <Route exact path="/chat" render={() => <Chat></Chat>}></Route>
         </Switch>
       </Router>
     </Container>
---
+++ src/pages/Chat.js
@@ -0,0 +1,13 @@
+export default function Chat() {
+  return (
+    <>
+      <div class="back"></div>
+      <div class="chat-header"></div>
+      <div class="chat-title"></div>
+      <div class="received-message"></div>
+      <div class="received-msg-timestamp"></div>
+      <div class="sent-message"></div>
+      <div class="sent-msg-timestamp"></div>
+    </>
+  );
+}
