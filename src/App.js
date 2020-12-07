69f2b92805a995f1665847873bbeaae7f9cf2552
Mehul Shinde <mehuldshinde@gmail.com>
2020-12-06 23:00:07 -0800

    Home

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import { Container } from 'react-bootstrap';
-import ItemList from './pages/ItemList.js';
-import VolunteerSignup from './pages/VolunteerSignup';
-import AvailabilityHealthStatus from './pages/Availability-HealthStatus';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles/App.css';
-import EditItems from './pages/EditItems';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles/App.css';
+import { Container } from "react-bootstrap";
+import ItemList from "./pages/ItemList.js";
+import VolunteerSignup from "./pages/VolunteerSignup";
+import AvailabilityHealthStatus from "./pages/Availability-HealthStatus";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/App.css";
+import EditItems from "./pages/EditItems";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/App.css";
 
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect,
-} from 'react-router-dom';
+} from "react-router-dom";
 
 function App() {
   return (
     <Container
       style={{
-        maxWidth: '576px',
-        height: '100vh',
-        margin: 'auto',
+        maxWidth: "576px",
+        height: "100vh",
+        margin: "auto",
       }}
     >
       <Router>
         <Switch>
-          <Route exact path="/" render={() => null}></Route>
+          <Route exact path="/" render={() => <div>HOME</div>}></Route>
           <Route exact path="/item-list" render={() => <ItemList />}></Route>
           <Route
             exact
